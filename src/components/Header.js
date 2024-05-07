d87e803a11c260a0ae67de74e92c48c604f67dcf
RupaliBarnwal <rupalibarnwal1797@gmail.com>
2024-05-07 19:09:08 +0530

    resolved URL issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
 
   // subscribing to the store using a selector, selector is the hook
   const cartItems= useSelector ((store)=> store.cart.items);
-  console.log(cartItems)
+  //console.log(cartItems)
 
   const onlineStatus= useOnlineStatus();
     return (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,15 @@
-export const LOGO_URL= 
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7jwxmZBKwVFeeo6Zc7qpn62zWvPe08pUQFQ&usqp=CAU";
+export const PROXY_URL = "https://cors-anywhere-5d5n.onrender.com/";
 
-export const CDN_URL=
-"https://corsproxy.org/?https%3A%2F%2Fmedia-assets.swiggy.com%2Fswiggy%2Fimage%2Fupload%2Ffl_lossy%2Cf_auto%2Cq_auto%2Cw_660%2F";
+export const CDN_URL =
 
-export const MENU_URL=
-"https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.6072861%26lng%3D77.2933104%26restaurantId%3D";
-export const REST_LINK=
-"https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.6072861%26lng%3D77.2933104%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7jwxmZBKwVFeeo6Zc7qpn62zWvPe08pUQFQ&usqp=CAU";
+
+export const MENU_URL =
+PROXY_URL +
+"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&catalog_qa=undefined&restaurantId=";
+export const REST_LINK =
+PROXY_URL +
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627"
